ffec57e5994110cdadbc13f8c8908c5d8b5ab0dc
Kevin Grondin <kevingrondin@outlook.com>
2017-08-14 16:51:15 +0400

    Update AccueilCtrl.js

--- vue/Accueil/AccueilCtrl.js
+++ vue/Accueil/AccueilCtrl.js
@@ -4,16 +4,17 @@
     angular.module('myWebSite')
     .controller('AccueilCtrl', MainController)
     
-    MainController.$inject = ['$scope','Info',];
+    MainController.$inject = ['$scope','Info'];
     function MainController($scope,Info) {
         $scope.Info = []
 
         function GetInfo(){ 
             Info.Get()
                 .then(function(data) {
-                    $scope.Info = data
+                    console.log(data);
+                    $scope.Info = data;
                 }, function(err) {
-                    console.log(res)
+                    console.log(err);
                 });
         }
 
