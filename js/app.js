4432b213b2f4f90e46a29bf3ecf28b3ef88e1522
Kevin Grondin <kevingrondin@outlook.com>
2017-08-14 16:32:48 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
     "use strict";
     
     angular.module('myWebSite', ['ngRoute','WebSite'])
+    .constant('CONSTANT', { JSON: "http://zonword.github.io/data.json" })
     .config(config)
     
     config.$inject = ['$routeProvider', '$httpProvider']
