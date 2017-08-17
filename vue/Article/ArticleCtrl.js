575be666d6d84dccbdc60d2a0cb89b11694b9d77
Kevin Grondin <kevingrondin@outlook.com>
2017-08-17 12:25:02 +0400

    Create ArticleCtrl.js

---
+++ vue/Article/ArticleCtrl.js
@@ -0,0 +1,12 @@
+(function(){
+    "use strict";
+    
+    angular.module('myWebSite')
+    .controller('ArticleCtrl', MainController)
+    
+    MainController.$inject = ['$scope'];
+    function MainController($scope) {
+       
+    }
+        
+})();
