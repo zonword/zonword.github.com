1dbeaab13ccbc735c7d82916ea535615f0d206cf
Kevin Grondin <kevingrondin@outlook.com>
2017-08-17 14:23:08 +0400

    Update AccueilCtrl.js

--- component/Accueil/AccueilCtrl.js
+++ component/Accueil/AccueilCtrl.js
@@ -6,19 +6,7 @@
     
     MainController.$inject = ['$scope','Info'];
     function MainController($scope,Info) {
-        $scope.Info = []
-
-        function GetInfo(){ 
-            Info.Get()
-                .then(function(data) {
-                    console.log(data.data.Information)
-                    $scope.Info = data.data.Information;
-                }, function(err) {
-                    console.log(err);
-                });
-        }
-
-        GetInfo();
+        
     }
         
 })();
