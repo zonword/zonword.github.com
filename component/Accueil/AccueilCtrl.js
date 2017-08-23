6bc482201446a35921ccc9491bf3c5c6bfbf6c44
zonword <kevingrondin@outlook.com>
2017-08-23 15:49:55 +0400

    Update AccueilCtrl.js

--- component/Accueil/AccueilCtrl.js
+++ component/Accueil/AccueilCtrl.js
@@ -6,7 +6,19 @@
     
     MainController.$inject = ['$scope','Info'];
     function MainController($scope,Info) {
-        
+        $scope.Info = []
+
+        function GetInfo(){ 
+            Info.Get()
+                .then(function(data) {
+                    console.log(data.data.Information)
+                    $scope.Info = data.data.Information;
+                }, function(err) {
+                    console.log(err);
+                });
+        }
+
+        GetInfo();
     }
         
 })();
